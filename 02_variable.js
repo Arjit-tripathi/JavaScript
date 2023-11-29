fca564b7050f55cd8160caaa1d7f9983531212e6
Arjit <arjittripathi1@gmail.com>
2023-11-29 10:32:29 +0530

    upload js file

---
+++ 02_variable.js
@@ -0,0 +1,26 @@
+const accountId = 12345
+let accountEmail = "arjittripathi1@gmail.com"
+var accountPassword = "111122"
+accountCity = "jaipur"
+
+let accountState;
+
+
+//accountId = 2 //not allowed
+
+accountEmail = "gopal@gmail.com"
+accountPassword = 32323
+accountCity = "Noida"
+
+//console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
+
+/*Note : 
+javascript mai const(Constant) and let ko use kiya jata hain,var ko nahi.
+
+var: block scope par iska koi controle nahi hota
+{}=>this scope
+
+
+*/
---
+++ 03_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all Js code as newer version
+//alert(4+5) // we are using node js,not broswer
+
+console.log(3+4)
+
+console.log("Hitesh")
+
+
+let name = "arjit"
+let age = 18 
+let isLoggedIn = false
+
+//number => 2 to power 53
+//BigInt
+//string => ""
+//Boolean => true/false
+//null => standlone value 
+//undefined = >
+//symbol=> unique
+//object
+
+//console.log(typeof "arjit");
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
