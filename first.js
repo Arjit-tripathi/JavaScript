0129f4dca351108a64f147db00e544c54f4ff275
Arjit Tripathi <arjittripathi1@gmail.com>
2024-08-07 08:58:51 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -1,9 +1,11 @@
 
-fullname= "Toney stark";
+fullName= "Toney stark";
 prise = 90;
 x = null; // iske under kuch bhi nahi hain
 y = undefined; //pata nahi iske endar kya hain
 
-console.log(fullname);
+//console.log(fullname);
+
 console.log(prise);
 
+//run terminal-> node first.js 
