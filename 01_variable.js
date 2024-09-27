653f2c51d98007064ce1289b58e7e04670ce401f
Arjit Tripathi <arjittripathi1@gmail.com>
2024-09-27 00:16:24 +0000

    container config done

---
+++ 01_variable.js
@@ -0,0 +1,11 @@
+const  accountId = 454545
+let accountEmail = "arjittripathi1@gmail.com"
+var accountPassword = "2222"
+
+//accountId = 9 //not change
+accountEmail = "demo@gmail.com"
+accountPassword ="444444444"
+
+//console.log(accountId);
+console.table([accountEmail,accountPassword]);
+
